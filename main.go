294b311f10beef2eeff205eb94075ba39d094971
Prajwal-Koirala-DEV <prajwalkoirala638@gmail.com>
2024-10-20 20:51:43 -0400

    Update main.go
    Signed-off-by: Prajwal-Koirala-DEV <prajwalkoirala638@gmail.com>

--- main.go
+++ main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/url"
 )
 
 /* Global variables */
 
 // List to the list of movies websites located in a file.
 var movies_websites_path string = "assets/movies-websites.txt"
-// List of the movies websites in a string.
-var movies_websites_content []string
 
 // The main function.
 func main() {
-	//
+	// Define a URL string to extract the domain from
+	urlContent := "https://fmovies.ps/"
+	// Call getDomainFromURL and print the extracted domain name
+	fmt.Println(getDomainFromURL(urlContent))
 }
 
+// getDomainFromURL extracts the domain name from the given URL string.
+func getDomainFromURL(givenURL string) string {
+	// Parse the given URL string into a URL structure
+	parsedUrl, err := url.Parse(givenURL)
+	if err != nil {
+		// Log an error message if parsing fails
+		log.Println(err)
+	}
+	// Return the hostname (domain name) from the parsed URL
+	return parsedUrl.Hostname()
+}
